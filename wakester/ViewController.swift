643d55da6984ab1c41b90b5e8c80713ed9e3a6bf
Brendan Stanton <bmsenate@gmail.com>
2018-01-15 09:40:53 -0800

    Initial Commit

---
+++ wakester/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  wakester
+//
+//  Created by Brendan Stanton on 1/15/18.
+//  Copyright © 2018 Brendan Stanton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
